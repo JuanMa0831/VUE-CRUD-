bcbc6ce846bc386a5f80edc329c31ef07a26bc4c
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-22 22:22:21 -0500

    Ajuste_Comunas.vue

--- src/views/Comunas.vue
+++ src/views/Comunas.vue
@@ -1,6 +1,11 @@
 <template>
     <div class= "container">
-        <h1>Listado de Comunas</h1>
+        <h1 class="text-start">Listado de Comunas
+        <button @click="newComuna()"
+        class="btn btn-succes mx-2">
+        <font-awesome-icon icon="plus" />
+    </button>
+</h1>
         <table class="table table-dark table-striped">
           <thead>
             <tr>
@@ -17,6 +22,14 @@
               <td>{{comuna.comu_nomb}}</td>
               <td>{{comun.muni_nomb}}</td>
             </tr>
+            <button @click="deleteComuna(comuna.comu_codi)"
+        class="btn btn-danger mx-2">
+        <font-awesome-icon icon="trash" />
+    </button>
+    <button @click="editComuna(comuna.comu_codi)"
+        class="btn btn-warning mx-2">
+        <font-awesome-icon icon="pencil" />
+    </button>
               </tbody>
         </table>
         </div>
@@ -24,6 +37,7 @@
     
         <script>
         import axios from 'axios'
+        import Swal from 'sweetalert2'
         
         export default {
             name: 'Comuna',
@@ -32,6 +46,31 @@
                     comunas: []
                 }
             },
+            methods:{
+                deleteComuna(codigo){
+                    Swal.fire({title: 'Estas seguro de eliminar la comuna con id ${codigo}?',
+                showCancelButton: true,
+                confirmButtonText: 'Eliminar',
+                }).then((result))=>{
+                    if (reult.isConfirmed){
+                        axios.delete('http://127.0.0.1:8000/api/comunas/${codigo}')
+                        .then(response=>){
+                            if(response.data.success){
+                                Swal.fire('Eliminar!!', '', 'Hecho')
+                                this.comunas = response.data.comunas
+                            }
+                        }
+                    }
+                }
+                }
+            },
+            editComuna(ide){
+                this.$router.push({name: 'EditarComuna', params: {id: '${id}'}})
+            },
+            newComuna(){
+                this.$router.push({name: 'NewComuna'});
+            }
+        
             mounted(){
                 axios
                 .get('http://127.0.0.1:8000/api/comunas')
